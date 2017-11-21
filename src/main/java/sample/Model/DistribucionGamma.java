a8963180b1719e8f5044f1bc58b79bb8d9ce3d47
B50770 <oscar.azofeifa.segura@gmail.com>
2017-11-21 12:40:52 -0600

    Merge branch 'master' of https://github.com/hilova/QueueSimulation into Oscar
    # Conflicts:
    #	src/main/java/sample/Model/DistribucionGamma.java

--- src/main/java/sample/Model/DistribucionGamma.java
+++ src/main/java/sample/Model/DistribucionGamma.java
@@ -15,7 +15,7 @@ public class DistribucionGamma implements Distribucion {
     public Duration calcular() {
         // TODO calcular distribucion
         Random generador = new Random();                                        //Inicializamos el generador de números aleatorios
-        long quantil = (long)Math.pow((Gamma.gamma(alfa)*generador.nextDouble())/(Math.pow(mu,alfa)*Math.exp(-mu)),1-alfa);  //Generamos el quantil a partir de la probabilidad generada
+        long quantil = (long)Math.pow((Gamma.gamma(alfa)*generador.nextDouble())/(Math.pow(mu,alfa)*Math.exp(-mu)),1/(alfa-1));  //Generamos el quantil a partir de la probabilidad generada
         return Duration.ofMinutes(quantil);
     }
 
