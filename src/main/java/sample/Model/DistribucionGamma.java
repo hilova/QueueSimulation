41b4e791c8a07b35c5b00ca5de427ac827a9902c
b53779 <roleandroflores@hotmail.com>
2017-11-21 12:19:43 -0600

    Eliminacion de comentarios TODO en exponencial y poisson Comentarios en gamma

--- src/main/java/sample/Model/DistribucionGamma.java
+++ src/main/java/sample/Model/DistribucionGamma.java
@@ -14,9 +14,9 @@ public class DistribucionGamma implements Distribucion {
 
     public Duration calcular() {
         // TODO calcular distribucion
-        Random generador = new Random();                                        //Inicializamos el generador de números aleatorios
+        /*Random generador = new Random();                                        //Inicializamos el generador de números aleatorios
         long quantil = (long)(-1 * Math.log(1-generador.nextDouble())/lambda);  //Generamos el quantil a partir de la probabilidad generada
-        return Duration.ofMinutes(quantil);
+        return Duration.ofMinutes(quantil);*/
         return Duration.ofHours(1);
     }
 
