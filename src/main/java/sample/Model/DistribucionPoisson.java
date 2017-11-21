da5daeab4c30dd01c55636ad79f7de24964e272b
B50770 <oscar.azofeifa.segura@gmail.com>
2017-11-21 11:22:09 -0600

    -Distribucion exponencial done! -Llenado de las otras distribuciones, por completar -Comentarios indexados -Función Gamma añadida

--- src/main/java/sample/Model/DistribucionPoisson.java
+++ src/main/java/sample/Model/DistribucionPoisson.java
@@ -1,16 +1,21 @@
 package sample.Model;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class DistribucionPoisson implements Distribucion{
-    private double mu;
+    private double lambda;
 
-    public DistribucionPoisson(double mu) {
-        this.mu = mu;
+    public DistribucionPoisson(double lambda) {
+        this.lambda = lambda;
     }
 
     public Duration calcular() {
         // TODO calcular distribucion
-        return Duration.ofHours(1);
+        Random generador = new Random();                                        //Inicializamos el generador de números aleatorios
+
+        long quantil = (long)(-1 * Math.log(1-generador.nextDouble())/lambda);  //Generamos el quantil a partir de la probabilidad generada
+        return Duration.ofMinutes(quantil);
     }
+
 }
